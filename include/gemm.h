784b415175a3e5d050c3dae95e94ac28197e4806
jangralakshay14 <jangralakshay14@gmail.com>
2025-11-22 23:53:54 +0530

    Updated Code

--- include/gemm.h
+++ include/gemm.h
@@ -8,5 +8,20 @@ using u32 = unsigned int;
 
 //CUDA error check
 #define CUDA_CHECK(call) do {
-
+cudaError_t err = (call);
+if(err != cudaSuccess){
+  fprintf(stderr, "CUDA ERROR %s:%d: %s\n",
+      __FILE__, __LINE__, cudaGetErrorString(err));
+  exit(EXIT_FAILURE);
 }
+} while(0)
+
+// Matrix descriptor
+
+struct Matrix {
+  f32 *ptr;   // device pointer
+  int rows;
+  int cols;
+  int ld;     // leading dimension (cols)
+
+};
